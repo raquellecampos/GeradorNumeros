650d2b290ea92a841cb2ecf6b66d0b2d548a0bb9
raquellecampos <raquellecampos12@gmail.com>
2021-06-15 14:50:15 -0400

    Adicionar arquivos de projeto.

---
+++ GeradorNumeros/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace GeradorNumeros {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RESULTADO = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(199, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(106, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(135, 26);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Primeiro";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ultimo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Quantidade";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // RESULTADO
+            // 
+            this.RESULTADO.AutoSize = true;
+            this.RESULTADO.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.RESULTADO.Location = new System.Drawing.Point(36, 155);
+            this.RESULTADO.MaximumSize = new System.Drawing.Size(250, 0);
+            this.RESULTADO.Name = "RESULTADO";
+            this.RESULTADO.Size = new System.Drawing.Size(127, 32);
+            this.RESULTADO.TabIndex = 6;
+            this.RESULTADO.Text = "Resultado";
+            this.RESULTADO.Click += new System.EventHandler(this.RESULTADO_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 45);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "GERAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 272);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RESULTADO);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label RESULTADO;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox textBox3;
+    }
+}
+
---
+++ GeradorNumeros/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorNumeros {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void RESULTADO_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            Random Aleatorio = new Random();
+            int Inicial = Convert.ToInt32(textBox1.Text);
+            int Final = Convert.ToInt32(textBox2.Text);
+            int Limite = Convert.ToInt32(textBox3.Text);
+
+            int Sorteados;
+            int[] verificador = new int[100];
+
+            String Resultado = "";
+
+            for (int i = 0; i <= Limite - 1; i++) {
+            Inicio:
+                Sorteados = Aleatorio.Next(Inicial, Final + 1);
+
+                for (int x = 0; x <= Limite -1; x++) {
+                    
+                    if (verificador[x] == Sorteados) {
+                        goto Inicio;
+                    }
+                }
+
+                Resultado = Resultado + " " + Sorteados ;
+                RESULTADO.Text = Resultado;
+                verificador[i] = Sorteados;
+            }
+        }
+
+       
+
+        private void label3_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
